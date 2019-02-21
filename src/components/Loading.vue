6e619b0782cd175b5f8a0c5977f08a991724ba40
vinnycl <carneiropresent@gmail.com>
2019-02-21 16:00:38 -0300

    Componente de Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loading">
+    <img src="../assets/images/galactic-empireo.svg" alt="Loading ...">
+    <img src="../assets/images/galactic-empirei.svg" alt="Loading ...">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Loading"
+  }
+</script>
+
+<style scoped lang="scss">
+    @keyframes spin { 
+        100% { 
+            transform:rotate(360deg);
+        }
+    }
+    @keyframes spinr { 
+        100% { 
+            transform:rotate(-360deg);
+        }
+    }
+    .loading {
+        position: absolute;
+        z-index: 10;
+        width: 200px;
+        height: 200px;
+        left: 50%;
+        top: 50%;
+        margin: -100px 0 0 -100px;
+        img {
+            position: absolute;
+            
+        }
+        img:nth-child(1) {
+            animation: spin 6s linear infinite;
+        }
+        img:nth-child(2) {
+            animation: spinr 6s linear infinite;
+        }
+    }
+</style>
