247a5d552e404824bd6921c51b96e6d4ed77c2d1
vinnycl <carneiropresent@gmail.com>
2019-02-22 01:13:10 -0300

    Pagina de erro

---
+++ src/components/Explosion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="explosion">
+        <button @click="onClick" ></button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Explosion",
+    props: {
+      onClick: {
+        type: Function,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+.explosion button {
+    position: fixed;
+    background: red;
+    z-index: 11;
+    width: 100px;
+    height: 30px;
+    bottom: 0px;
+    left: 0px;
+}
+</style>
--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-      404
+  <div class="error">
+
+      <div class="error-content">
+        <h1>It's a trap! An error has occurred!</h1>
+        <video autoplay playsinline width="640" height="320"  >
+          <source src="https://vinnycl.github.io/statics/videos/error.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      
+
+      <universe></universe>
   </div>
 </template>
 
 <script>
-
+import Universe from '@/components/Universe.vue';
+export default {
+  components: {
+      Universe
+  }
+}
 </script>
 
-<style>
+<style scoped lang="scss">
+
+.error {
+  color: rgb(75, 213, 238);
+  .error-content {
+    text-align: center;
+    position: absolute;
+    z-index: 10;
+    width: 100%;
+    top: 50%;
+    margin-top: -200px;
+  }
+}
+
 </style>
