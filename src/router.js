2dfd6950221ee9c6e70f7ad98a585cf164b9e2d0
vinnycl <carneiropresent@gmail.com>
2019-02-22 01:14:00 -0300

    Nova rota de pagina para introducao

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Deathstar from './views/Deathstar.vue';
 import Error from './views/Error.vue';
+
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home
+        },
+        {
+            path: '/deathstar',
+            name: 'deathstar',
+            component: Deathstar
         },
         {
             path: '/error',
